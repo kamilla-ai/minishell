61a56d4aaaf2125b2669da482bc90cfd102d26b7
Taha_Helmy <153909093+Thelmi@users.noreply.github.com>
2024-08-19 16:52:47 +0400

    Environment struct and functions added

--- minishell.h
+++ minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: krazikho <krazikho@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mrhelmy <mrhelmy@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/14 14:58:51 by krazikho          #+#    #+#             */
-/*   Updated: 2024/08/19 15:33:22 by krazikho         ###   ########.fr       */
+/*   Updated: 2024/08/19 16:13:06 by mrhelmy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,28 @@
 #include <readline/history.h>
 #include <stdbool.h>
 #include <stdlib.h>
+
+// our environment struct
+typedef struct env
+{
+	char *variable;
+	char *value;
+	struct env *next;
+} t_env;
+
+// Functions used to store the enironment in the struct
+
+t_env   *create_env_Nodes(char *variable_content, char *value_content); // warning malloc used
+t_env   *storing_env(char **ev); // warning createNodes used here which uses malloc
+void    free_env(t_env *env);
+
+// env command (printing the environment)
+
+void    env(t_env *env);
+
+// We might use them, globally
+char    *substr_before_char(char *str , char c); // warning malloc is used
+char    *substr_after_char(char *str , char c); // warning malloc is used
 
 void execute_command(char *command);
 int	ft_strcmp(char *s1, char *s2);
