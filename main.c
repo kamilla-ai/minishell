71c27c23cdde6e12cd73ecd9a843e14004eee275
kamilla-ai <107996427+kamilla-ai@users.noreply.github.com>
2024-08-20 16:18:32 +0400

    Update main.c

--- main.c
+++ main.c
@@ -6,14 +6,18 @@
 /*   By: krazikho <krazikho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/14 14:58:22 by krazikho          #+#    #+#             */
-/*   Updated: 2024/08/14 14:58:26 by krazikho         ###   ########.fr       */
+/*   Updated: 2024/08/20 16:17:39 by krazikho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int main(){
+int main(int ac, char **av, char **ev){
+    (void)ac;
+    (void)av;
     char *command;
+    t_env *envir;
+    envir = storing_env(ev);
     while(1)
     {
         command = readline("minishell$ ");
@@ -22,7 +26,8 @@ int main(){
         }
         if (*command) {
             add_history(command);
-            execute_command(command);
+            
+            execute_command(command, envir);
         }
         
         free(command);
