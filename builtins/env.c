404dcf516b7a6b580c96d6aacfb3463deab09a9a
Taha_Helmy <153909093+Thelmi@users.noreply.github.com>
2024-08-20 17:57:17 +0400

    norminette is fixed

--- builtins/env.c
+++ builtins/env.c
@@ -3,30 +3,30 @@
 /*                                                        :::      ::::::::   */
 /*   env.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mrhelmy <mrhelmy@student.42.fr>            +#+  +:+       +#+        */
+/*   By: thelmy <thelmy@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/17 21:26:02 by mrhelmy           #+#    #+#             */
-/*   Updated: 2024/08/19 16:23:02 by mrhelmy          ###   ########.fr       */
+/*   Updated: 2024/08/20 17:46:02 by thelmy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-void env(t_env *env)
+void	env(t_env *env)
 {
-	t_env  *tmp;
+	t_env	*tmp;
+
 	if (!env)
-    {
-        printf("Environment is empty or not initialized.\n");
-        return;
-    }
-	tmp = env;
-	while(tmp != NULL)
 	{
-		printf("%s", tmp->variable);
-		printf("=");
-		printf("%s\n", tmp->value);
+		printf ("Environment is empty or not initialized.\n");
+		return ;
+	}
+	tmp = env;
+	while (tmp != NULL)
+	{
+		printf ("%s", tmp->variable);
+		printf ("=");
+		printf ("%s\n", tmp->value);
 		tmp = tmp->next;
 	}
 }
-
