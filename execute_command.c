413ef492fdf9fc41f7aa6f3fc12628a4f7d22814
kamilla-ai <107996427+kamilla-ai@users.noreply.github.com>
2024-08-20 16:19:18 +0400

    Update execute_command.c

--- execute_command.c
+++ execute_command.c
@@ -1,42 +1,35 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   execute_command.c                                  :+:      :+:    :+:   */
+/*   execute_builtin.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: krazikho <krazikho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/08/19 14:27:04 by krazikho          #+#    #+#             */
-/*   Updated: 2024/08/20 16:17:05 by krazikho         ###   ########.fr       */
+/*   Created: 2024/08/19 14:55:09 by krazikho          #+#    #+#             */
+/*   Updated: 2024/08/20 15:59:48 by krazikho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-
-static bool is_executable(char *command){
-    (void)command;
-    return false;
-}
-
-static bool is_builtin(char *command){
-    if(ft_strcmp("echo", command)==true||ft_strcmp("cd", command)==true || ft_strcmp("pwd", command)==true) {
-        return true;
-    }else if(ft_strcmp("export", command)==true || ft_strcmp("unset", command)==true || ft_strcmp("env", command)==true){
-        return true;
-    }else if(ft_strcmp("exit", command)==true){
-        return true;
-    }else{
-        return false;
-    }
-}
-
-void execute_command(char *command, t_env *env){
-    if(is_builtin(command)==true){
-        execute_builtin(command, env);
-    }else if(is_executable(command)==true){
+void execute_builtin(char *command, t_env *envir){
+    if(ft_strcmp("echo", command)==true){
         ;
-        // execute_external(command);
-    }else{
-        printf("bash: command not found: %s", command);
-        exit(1);
+        // echo();
+    }else if(ft_strcmp("cd", command)==true){
+        ;
+        // cd();
+    }else if(ft_strcmp("pwd", command)==true){
+        pwd();
+    }else if(ft_strcmp("export", command)==true){
+        ;
+        // export();
+    }else if(ft_strcmp("unset", command)==true){
+        ;
+        // unset();
+    }else if(ft_strcmp("env", command)==true){
+        env(envir);
+    }else if(ft_strcmp("exit", command)==true){
+        ;
+        // exit();
     }
 }
