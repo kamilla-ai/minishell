265875c4d725f821a528facc850dcedf78722743
kamilla-ai <107996427+kamilla-ai@users.noreply.github.com>
2024-09-13 21:03:45 +0400

    Update string_functions.c

--- utils/string_functions.c
+++ utils/string_functions.c
@@ -14,6 +14,25 @@
 
 // functions from libft... temperorally, until we upload a libft to our minishell project 
 
+char *ft_strcat(char *dest, const char *src) {
+    int i = 0;
+    int j = 0;
+
+    while (dest[i] != '\0') {
+        i++;
+    }
+
+    while (src[j] != '\0') {
+        dest[i] = src[j];
+        i++;
+        j++;
+    }
+
+    dest[i] = '\0';
+
+    return dest;
+}
+
 bool	ft_strcmp(char *s1, char *s2)
 {
 	int	i;
